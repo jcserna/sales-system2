50cf0f9da0db2540213696392f0ff64a1e91d2a1
jcserna <jcserna87880@umanizales.edu.co>
2021-03-02 15:35:45 -0500

    images added

--- Sales-system/Program.cs
+++ Sales-system/Program.cs
@@ -6,49 +6,78 @@ namespace Sales_system
     {
         static void Main(string[] args)
         {
+
+            Store store = new Store();
+            
             SalesMan salesMan1 = new SalesMan();
             salesMan1.Name = "Juan";
             salesMan1.Age = 22;
+            store.AddSalesMan(salesMan1);
 
             SalesMan salesMan2 = new SalesMan();
             salesMan2.Name = "Luis";
             salesMan2.Age = 30;
+            store.AddSalesMan(salesMan2);
 
             SalesMan salesMan3 = new SalesMan();
             salesMan3.Name = "Andrea";
             salesMan3.Age = 28;
+            store.AddSalesMan(salesMan3);
 
             Product p1 = new Product();
-            p1.Name = "Computer";
+            p1.Name = "computer";
             p1.Price = 4327167;
+            store.AddProducts(p1);
 
             Product p2 = new Product();
-            p2.Name = "Earphones";
+            p2.Name = "earphones";
             p2.Price = 699000;
+            store.AddProducts(p2);
 
             Product p3 = new Product();
-            p3.Name = "Cellphone";
+            p3.Name = "cellphone";
             p3.Price = 1546985;
+            store.AddProducts(p3);
 
             Sale s1 = new Sale();
             s1.SalesMan = salesMan3;
             s1.Product = p3;
             s1.Comments = "Venta de celular";
+            store.AddSales(s1);
 
             Sale s2 = new Sale();
             s2.SalesMan = salesMan2;
             s2.Product = p2;
-            s2.Comments = "Buen producto";
+            s2.Comments = "producto antiguo";
+            store.AddSales(s2);
 
             Sale s3 = new Sale();
             s3.SalesMan = salesMan1;
             s3.Product = p1;
             s3.Comments = "Producto nuevo";
+            store.AddSales(s3);
 
-            double salesTotalAmount = s1.Product.Price + s2.Product.Price + s3.Product.Price;
 
-            System.Console.WriteLine("El valor de las ventas es de: " + salesTotalAmount + " pesos");
-            
+
+            //   double salesTotalAmount = s1.Product.Price + s2.Product.Price + s3.Product.Price;
+            //  System.Console.WriteLine("El valor de las ventas es de: " + salesTotalAmount + " pesos");
+
+
+
+
+
+            //string SellerCheapest = store.SellerCheapest(store.Sales);
+            //System.Console.WriteLine("El vendedor que vendió el producto mas barato fue: " + SellerCheapest);
+
+            //double Average = store.PriceAverage(store.Products);
+            //System.Console.WriteLine("El precio promedio de los productos es: " + Average);
+
+
+            //System.Console.WriteLine("De este producto se han vendido " + store.QuantityProductSold(store.Sales) + " unidades");
+
+            Product MostExpensiveProduct = store.MostExpensiveProduct(store.Products);
+            System.Console.WriteLine("El producto más caro es " + MostExpensiveProduct.Name + " con un precio de $" + MostExpensiveProduct.Price);
+
         }
     }
 }
---
+++ Sales-system/Store.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales_system
+{
+    class Store
+    {
+        public SalesMan[] SalesMen = new SalesMan[3];
+        int salesMenCount = 0;
+        public Product[] Products = new Product[20];
+        int productsCount = 0;
+        public Sale[] Sales = new Sale[12];
+        int salesCount = 0;
+
+        public void AddSalesMan(SalesMan newSalesMan)
+        {
+            SalesMen[salesMenCount] = newSalesMan;
+            salesMenCount++;
+        }
+        public void AddProducts(Product newProduct)
+        {
+            Products[productsCount] = newProduct;
+            productsCount++;
+        }
+        public void AddSales(Sale newSale)
+        {
+            Sales[salesCount] = newSale;
+            salesCount++;
+        }
+
+        //Who's the seller (sales man name), who has sold the cheapest product.
+        public string SellerCheapest(Sale[] sales)
+        {
+            double cheapestPrice = sales[0].Product.Price;
+            string Seller = "";
+            for (int i = 0; i < salesCount; i++)
+            {
+                if (Sales[i].Product.Price <= cheapestPrice)
+                {
+                    cheapestPrice = Sales[i].Product.Price;
+                    Seller = Sales[i].SalesMan.Name;
+                }
+            }
+            
+            return  Seller;
+        }
+        
+        //Products price average.
+        public double PriceAverage(Product[]products)
+        {
+            double sumPrices=0;
+            double average;
+            for (int i = 0; i < productsCount; i++)
+            {
+                sumPrices = sumPrices + products[i].Price;
+            }
+            average = sumPrices / productsCount;
+            return average;
+        }
+
+        //How many unit of the product has been sold.
+        public int QuantityProductSold(Sale[] sales)
+        {
+            Console.WriteLine("Ingrese el nombre de producto a consultar");
+            string ProductName=Console.ReadLine().ToLower();
+            int ProductSoldCount = 0;
+            for (int i = 0; i < salesCount; i++)
+            {
+                if (sales[i].Product.Name == ProductName)
+                {
+                    ProductSoldCount++;
+                }
+            }
+            return ProductSoldCount;
+        }
+
+        //What's the most expensive product.
+        public Product MostExpensiveProduct(Product[] products)
+        {
+            Product expensiveProduct = products[0];
+            for (int i = 0; i < productsCount; i++)
+            {
+                if (products[i].Price >= expensiveProduct.Price)
+                {
+                    expensiveProduct = products[i];
+                }
+            }
+            return expensiveProduct;
+        }
+    }
+}
